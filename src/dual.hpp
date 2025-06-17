0c53fda21f6f3c391c5f5e0f89245e4a732c9e14
Anton Kovalov <anton.kov.hack@proton.me>
2025-06-17 23:32:25 +0200

    docs: adds explanation to the core types

--- src/dual.hpp
+++ src/dual.hpp
@@ -1,6 +1,32 @@
 #pragma once
 #include <string>
 
+/**
+ * A data type that implements a dual floating-point
+ * arithmetic. The term "dual" means that the derivatve
+ * computation is performed alongside the original computation.
+ *
+ * Mathematically this is possible because of the chain rule
+ * of differentiation.
+ *
+ * The dual class contains two fields - val and der.
+ * Computations done on the val filed are the same as the
+ * computations that would have been done on a regular real-valued
+ * variable. Computations done on the der field are the dual
+ * derivative computations.
+ *
+ * Usage:
+ *
+ * // Declare a variable wrt to which the derivative is desired
+ * dual x = dual::active(42.0);
+ *
+ * // Due to the overloaded * operator, the derivative is also computed as
+ * // 2 * x, and is stored in the der filed of the f variable.
+ * dual f = x * x;
+ *
+ * // Prints 84.0 or df/dx = 2 * x = 2 * 42 = 84.
+ * std::cout << f.der << std::endl;
+ */
 class dual {
 public:
   double val;
@@ -8,16 +34,52 @@ public:
 
   dual(double val, double der = 0.0) : val(val), der(der){};
 
+  /**
+   * A factory method that instantiates an acive value, derivatives
+   * with respect to which will be propagated.
+   *
+   * @param val - a regular value of a variable
+   * @return dual, which can be used to propagate derivatives
+   */
   static dual active(double val);
+
+  /**
+   * A factory method that instantiates a passive value, derivatives
+   * with respect to which will NOT be propagated.
+   */
   static dual passive(double val);
 
   std::string to_str();
 
+  /**
+   * Add two FP values in a dual manner
+   *
+   * c = a + b
+   * dc = da + db
+   */
   dual operator+(dual other);
+
+  /**
+   * Subtract two FP values in a dual manner
+   *
+   * c = a - b
+   * dc = da - db
+   */
   dual operator-(dual other);
+
+  /**
+   * Multiply two FP values in a dual manner
+   *
+   * c = a * b
+   * dc = b*da + a*db
+   */
   dual operator*(dual other);
 };
 
+/**
+ * Some standard mathematics functions overloaded for the dual
+ * computation with the dual data type.
+ */
 namespace std {
 dual sin(dual x);
 dual cos(dual x);
--- src/rosenbrock.hpp
+++ src/rosenbrock.hpp
@@ -1,14 +1,30 @@
 #pragma once
-#include <cmath>
 #include <array>
+#include <cmath>
 
-template <typename T> class rosenbrock {
+/**
+ * Rosenbrock function - https://en.wikipedia.org/wiki/Rosenbrock_function.
+ */
+template <typename T = double> class rosenbrock {
 private:
   float a;
   float b;
 
 public:
+  /**
+   * @param a - a function parameter, from which the extremum point can be
+   * inferred.
+   * @param b - a function parameter, from which the difficulty to obtain the
+   * extremum can be inferred.
+   */
   rosenbrock(float a, float b) : a(a), b(b){};
+
+  /**
+   * An overloaded () operator to evaluate the function at a pair of points
+   * (x_0, x_1).
+   * @param x_vec - a 2D argument at which the function is evaluated
+   * @return the result of the evaluation
+   */
   T operator()(std::array<T, 2> x_vec) {
     T x = x_vec[0], y = x_vec[1];
     return std::pow(x - a, 2) + std::pow(y - x * x, 2) * b;
